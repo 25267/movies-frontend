91f82b44bb6d5653305a00be62b36f4d91883444
25267 <zhanibekamangeldiev@gmail.com>
2021-10-24 19:47:46 +0500

    frontend

---
+++ src/main/java/com/example/moviefrontendmicroservice/MovieFrontendMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviefrontendmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieFrontendMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieFrontendMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/moviefrontendmicroservice/MovieFrontendMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviefrontendmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieFrontendMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
